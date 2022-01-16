9c782c8d125f1b1776cf30996acd155284708382
godofdepression <97756402+godofdepression@users.noreply.github.com>
2022-01-17 03:29:49 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/project'));
+app.use(express.static('./dist/Mark'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/project/'}),
+    res.sendFile('index.html', {root: 'dist/Mark/'}),
 );
 
 // Start the app by listening on the default Heroku port
