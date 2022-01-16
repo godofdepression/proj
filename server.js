5ec35fde87f833f8ed2c2ee8a900ce2283f16644
godofdepression <97756402+godofdepression@users.noreply.github.com>
2022-01-17 02:05:06 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__proj + "/dist"));
+app.use(express.static(proj + "/dist"));
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__proj, "dist", "index.html"));
+  res.sendFile(path.join(proj, "dist", "index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
