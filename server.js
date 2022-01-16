d1506c97931b88d112719ecebec312fe2a025988
Abdul Rafay <191401@students.au.edu.pk>
2022-01-17 01:26:07 +0500

    initial commit

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
 const express = require('express');
-const app = express();
-const app = express();
-app.use(requireHTTPS);
+const path = require('path');
 
-app.use(express.static('./dist/<package.json>'));
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/<package.json>/'}
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/proj'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('app.component.html', {root: 'dist/proj/'}),
 );
-});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
