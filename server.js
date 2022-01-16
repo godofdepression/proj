16429a71b483ae13ffa56df6cb03d7be5d2c01cb
Abdul Rafay <191401@students.au.edu.pk>
2022-01-16 23:19:46 +0500

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-//Install express server
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
-const path = require('path');
-
 const app = express();
+const app = express();
+app.use(requireHTTPS);
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/proj'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/proj/'}),
+app.use(express.static('./dist/<package.json>'));
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/<package.json>/'}
 );
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
